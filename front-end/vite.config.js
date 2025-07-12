3ee227fb9b48036fe6b7047d36c83e32d2e412d9
itamarHenrique <itamarhjp1@gmail.com>
2025-07-12 00:14:44 -0300

    separa frontend em pasta e isola Laravel para deploy

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+export default {
+  root: './',
+  build: {
+    outDir: '../public/build', // saída no Laravel
+    emptyOutDir: true,
+  },
+  publicDir: '../public',
+}
