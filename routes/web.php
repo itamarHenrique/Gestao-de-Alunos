822d7e6aa8d1a249d1b56923e833169c15fa6504
itamarHenrique <itamarhjp1@gmail.com>
2025-07-12 10:36:45 -0300

    teste 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migração executada!';
 });
